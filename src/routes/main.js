1b45d1bc71f9774ea514c14f787cb7094e0f9866
mohammadali <mahammadalisunasara06@gmail.com>
2023-03-15 21:24:01 +0530

    ddd

--- src/routes/main.js
+++ src/routes/main.js
@@ -611,7 +611,7 @@ const sendWebgBookingMail = async (email, name, budget, businesstype) => {
 }
 //web development booking
 
-routers.get('/developmentBooking', auth, (req, resp) => {
+routers.get('/developmentBooking',(req, resp) => {
     const isLoggedIn = req.session.isLoggedIn || false;
     return resp.render('developmentBooking', { isLoggedIn });
 });
